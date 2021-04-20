232be04983682276e71bbed1f53484f68f728674
Graci7 <81484387+Graci7@users.noreply.github.com>
2021-04-20 19:09:57 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+var tempoDaCaminhada = prompt("Quanto tempo você caminhou?")
+
+var tempoDaCaminhadaNumero = parseInt(tempoDaCaminhada)
+
+var CaloriaGasta = tempoDaCaminhadaNumero * 5
+
+alert(CaloriaGasta)
